3cbd20d038677b41f5efad3192a46f0cd7379840
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-08-24 12:16:58 -0700

    functions to print char and string

---
+++ more_print.c
@@ -0,0 +1,51 @@
+#include "monty.h"
+
+/**
+ * printCharFromTop - prints the char at the top of the stack, followed by a new line
+ * @stack: pointer to head
+ * @line_number: current line
+ * Return: void
+ */
+
+void printCharFromTop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (temp == NULL)
+	{
+		free(global.content);
+		fclose(global.file);
+		free_stack(stack);
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (temp->n >= 128 || temp->n < 0)
+	{
+		free(global.content);
+		fclose(global.file);
+		free_stack(stack);
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", temp->n);
+}
+
+/**
+ * printStringFromTop - prints the string starting at the top of the stack
+ * @stack: pointer to head
+ * @line_number: current line
+ * Return: void
+ */
+
+void printStringFromTop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (line_number)
+		while ((temp && temp->n != 0) && (temp->n > 0 && temp->n < 128))
+		{
+			printf("%c", temp->n);
+			temp = temp->next;
+		}
+	printf("\n");
+}
